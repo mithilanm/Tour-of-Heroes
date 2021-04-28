ec34b596ed111540f3ea091f26ce742e3a8200c0
mithilanm <mithilan_26@hotmail.com>
2021-04-28 14:16:55 -0400

    Get Data from a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -10,7 +10,9 @@ import { Location } from '@angular/common';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?:Hero;
+  
+  hero!: Hero;
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -29,5 +31,10 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(()=> this.goBack());
   }
 }
